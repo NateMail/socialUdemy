ca1aba324a4188f14f4ba6dd3ff1fc654ac7b6aa
nathanmailhiot <nathanjm12@outlook.com>
2019-10-29 12:10:19 -0400

    Refactor

--- routes/post.js
+++ routes/post.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const { getPosts, createPost } = require('../controllers/post');
-const validator = require('../controllers/validator');
+const { createPostValidator } = require('../controllers/validator');
 
 const router = express.Router();
 
 router.get('/', getPosts);
-router.post('/post', validator.createPostValidator, createPost);
+router.post('/post', createPostValidator, createPost);
 
 module.exports = router;
