8b5c32254bcfd858a6db2f4fa6ad541ffc5c23c6
nathanmailhiot <nathanjm12@outlook.com>
2019-10-29 08:59:21 -0400

    Connect to mongodb

--- app.js
+++ app.js
@@ -1,8 +1,21 @@
 const express = require('express');
-
 const app = express();
+const mongoose = require('mongoose');
 const morgan = require('morgan');
+const dotenv = require('dotenv');
+dotenv.config();
 
+//db
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+  .then(console.log('DB connected'));
+
+mongoose.connection.on('error', err => {
+  console.log(`DB connection error: ${err.message}`);
+});
 // bring in routes
 const postRoutes = require('./routes/post');
 
@@ -11,7 +24,7 @@ app.use(morgan('dev'));
 
 app.use('/', postRoutes);
 
-const port = 8080;
+const port = process.env.PORT || 8080;
 app.listen(port, () => {
   console.log(`A Node JS Api is listening on port: ${port}`);
 });
