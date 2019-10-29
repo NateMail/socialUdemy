3139e5b26a1b86303775be1d2e38e0d98809e938
nathanmailhiot <nathanjm12@outlook.com>
2019-10-29 10:40:04 -0400

    Create post

--- controllers/post.js
+++ controllers/post.js
@@ -1,5 +1,21 @@
+const Post = require('../models/post');
+
 exports.getPosts = (req, res) => {
   res.json({
     posts: [{ title: 'First Post' }, { title: 'Second Post' }]
+  });
+};
+
+exports.createPost = (req, res) => {
+  const post = new Post(req.body);
+  post.save((err, result) => {
+    if (err) {
+      return res.status(400).json({
+        error: err
+      });
+    }
+    res.status(200).json({
+      post: result
+    });
   });
 };
