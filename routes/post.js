05c755c45db8a35faeab996022cd4ac41f9dd1ee
nathanmailhiot <nathanjm12@outlook.com>
2019-10-30 10:40:55 -0400

    Able to delete post

--- routes/post.js
+++ routes/post.js
@@ -1,5 +1,12 @@
 const express = require('express');
-const { getPosts, createPost, postsByUser } = require('../controllers/post');
+const {
+  getPosts,
+  createPost,
+  postsByUser,
+  postById,
+  isPoster,
+  deletePost
+} = require('../controllers/post');
 const { requireSignin } = require('../controllers/auth');
 const { userById } = require('../controllers/user');
 const { createPostValidator } = require('../controllers/validator');
@@ -14,8 +21,11 @@ router.post(
   createPostValidator
 );
 router.get('/posts/by/:userId', postsByUser);
+router.delete('/post/:postId', requireSignin, isPoster, deletePost);
 
 // any route containing :userId, our app will first execute userById()
 router.param('userId', userById);
+
+router.param('postId', postById);
 
 module.exports = router;
