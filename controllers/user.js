721019a4b081481905041b44b1be55c3124fa2ef
nathanmailhiot <nathanjm12@outlook.com>
2019-10-30 08:33:16 -0400

    Update User

--- controllers/user.js
+++ controllers/user.js
@@ -1,3 +1,4 @@
+const _ = require('lodash');
 const User = require('../models/user');
 
 exports.userById = (req, res, next, id) => {
@@ -37,4 +38,21 @@ exports.getUser = (req, res) => {
   req.profile.hashed_password = undefined;
   req.profile.salt = undefined;
   return res.json(req.profile);
+};
+
+exports.updateUser = (req, res, next) => {
+  let user = req.profile;
+  // extend - mutate the source object
+  user = _.extend(user, req.body);
+  user.updated = Date.now();
+  user.save(err => {
+    if (err) {
+      return res.status(400).json({
+        error: 'You are not authorized to preform this action'
+      });
+    }
+    user.hashed_password = undefined;
+    user.salt = undefined;
+    res.json({ user });
+  });
 };
